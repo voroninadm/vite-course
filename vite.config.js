0a22732eed04e4233d3e451b9537329aebd03ba9
voroninadm <dev@Rince-devLap.>
2025-01-30 11:35:42 +0300

    update deploy

--- vite.config.js
+++ vite.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
         })
     ],
     build: {
-        outDir: '../dist',
+        // outDir: '../dist',
         minify: true,
         minifyCSS: "esbuild/lightningcss",
         sourcemap: true,
